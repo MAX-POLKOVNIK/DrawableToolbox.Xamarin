7e388312eb9454966a1bf9d749194aad75374d9d
Max Polkovnik <max-polkovnik@mail.ru>
2019-01-07 00:35:02 +0700

    Initial commit

---
+++ DrawableToolbox.Sample/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Top.Defaults.Drawabletoolbox;
+using Android.Graphics;
+
+namespace DrawableToolbox.Sample
+{
+    [Activity(Label = "DrawableToolbox.Sample", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            var background = new DrawableBuilder()
+                .Rectangle()
+                .HairlineBordered()
+                .StrokeColor(Color.Red)
+                .StrokeColorPressed(Color.Blue)
+                .Ripple()
+                .RippleColor(Color.Yellow)
+                .Build();
+
+            button.Background = background;
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
---
+++ DrawableToolbox/Additions/DrawableBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Java.Lang;
+
+namespace Top.Defaults.Drawabletoolbox
+{
+    public partial class DrawableBuilder
+    {
+        public DrawableBuilder GradientColors(int startColor, int endColor, int centerColor) => 
+            GradientColors(startColor, endColor, Integer(centerColor));
+
+        public DrawableBuilder SolidColorPressed(int color) => SolidColorPressed(Integer(color));
+
+        public DrawableBuilder SolidColorPressedWhenRippleUnsupported(int color) => SolidColorPressedWhenRippleUnsupported(Integer(color));
+
+        public DrawableBuilder SolidColorDisabled(int color) => SolidColorDisabled(Integer(color));
+
+        public DrawableBuilder SolidColorSelected(int color) => SolidColorSelected(Integer(color));
+
+        public DrawableBuilder StrokeColorPressed(int color) => StrokeColorPressed(Integer(color));
+
+        public DrawableBuilder StrokeColorDisabled(int color) => StrokeColorDisabled(Integer(color));
+
+        public DrawableBuilder StrokeColorSelected(int color) => StrokeColorSelected(Integer(color));
+
+        private static Integer Integer(int csharpInt) => new Integer(csharpInt);
+    }
+}
